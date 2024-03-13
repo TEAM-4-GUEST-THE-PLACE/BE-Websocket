03eaa198241e4725277e1d3e2921129c3772560a
Iqbal Pradipta <iqbalpradipta2@gmail.com>
2024-03-13 15:57:07 +0700

    feat: websocket

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import { Server } from 'socket.io';
@@ -12,6 +12,22 @@ const io = new Server(server, {
     origin: 'http://localhost:8001',
     methods: ['GET', 'POST'],
   },
+});
+
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = {
+      id: socket.id,
+      username,
+      room,
+    }
+    socket.join(user.room)
+  });
+
+  socket.on("sendData", (data) => {
+    socket.to(data.room).emit("receiveData", data)
+  })
+
 });
 
 server.listen(8001, () => {
